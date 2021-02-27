152af8ff6c947a5c390c49aa644a272fc5024278
Justin Kenel <jfkenel@gmail.com>
2021-02-27 16:23:26 -0500

    Update texasSearch.js

--- Texas/texasSearch.js
+++ Texas/texasSearch.js
@@ -5,7 +5,10 @@ const getTexasOfficeID = function(){
   request({
     uri:'https://candidate.texas-election.com/Elections/getQualifiedCandidatesInfo.do',
     method:'POST',
-    jar:request.cookie('coo')
+    jar:request.cookie('coo'),
+    headers: {
+      'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36"
+    }
   }, (e,r,b)=>{
     if(e) return e;
     console.log(b);
