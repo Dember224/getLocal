bf0c679f9850812a28889597a8a30328178b7a66
Antoine <Dember224@gmail.com>
2021-03-06 02:11:22 -0500

    Adding finance totals for 2020 campaigns Texas

--- Texas/texasSearch.js
+++ Texas/texasSearch.js
@@ -1,6 +1,11 @@
 const request = require('request');
 const cheerio = require('cheerio');
 const async = require('async');
+const fs = require('fs');
+const pdf = require('pdf-parse');
+const crawler = require('crawler-request');
+
+
 
 const getTexasElectionID = function(callData, callback){
   request({
@@ -53,7 +58,7 @@ const getTexasElectionID = function(callData, callback){
 };
 
 
-const getTexasCandidateNames = function(callData){
+const getTexasCandidateNames = function(callData, callback){
   async.autoInject({
     get_election_id: (cb)=>{
       getTexasElectionID({year:2020,optionSearch:'#idElection'}, (e,election_id)=>{
@@ -69,9 +74,8 @@ const getTexasCandidateNames = function(callData){
     }
   }, (e,r)=>{
     if(e) return e;
-    const candidate_object = {}
+    const candidate_array = []
     async.eachOf(r.get_offices.senate_offices, function(office_id,office_name,cb){
-      console.log(office_name, office_id)
       request({
         uri:'https://candidate.texas-election.com/Elections/getQualifiedCandidatesInfo.do',
         method:'POST',
@@ -81,7 +85,7 @@ const getTexasCandidateNames = function(callData){
           'content-type': 'application/x-www-form-urlencoded',
         },
         qs:{
-          nbElecYear: callData.year || 2020,
+          nbElecYear: callData.year,
           cdParty:'D',
           cdOfficeType: 'S',
           cdFlow: 'S',
@@ -94,14 +98,55 @@ const getTexasCandidateNames = function(callData){
         if(e) return cb(e);
         let $ = cheerio.load(b);
         const candidate_name = $('p strong').first().text();
-        console.log(candidate_name)
+        district_array = office_name.split(',');
+        candidate_object = {
+          name: candidate_name,
+          office:district_array[0],
+          district:district_array[1],
+          state: 'Texas'
+        }
+        candidate_array.push(candidate_object)
+        cb();
       })
     },(e,r)=>{
-      if(e) return e;
-      console.log(r)
+      if(e) return callback(e);
+      return callback(null,candidate_array)
     } )
   })
 }
 
+const getCandidateMoney = function(){
+  async.autoInject({
+    candidates:(cb)=>{
+      getTexasCandidateNames({year:2020},(e,candidate)=>{
+        if(e) return e;
+        return cb(null, candidate)
+      })
+    },
+    pdf:(cb)=>{
+      crawler('https://www.ethics.state.tx.us/data/search/cf/2020/CandE012020.pdf').then(function(response){
+        const report_array = response.text.split("Report Due:1/15/20Report Number:");
+        const num_of_reports = response.text.split("Report Due:1/15/20Report Number:").length - 1;
+        console.log(`There are ${num_of_reports} candidates and pacs in this file`);
+        return cb(null, report_array)
+      })
+    }
+  },(e,r)=>{
+    if(e) return e;
+    r.candidates.forEach(candidate=>{
+      if(candidate.name.length){
+        const name_split = candidate.name.split(" ");
+        const first_name = name_split[0]
+        const last_name = name_split[name_split.length - 1]
+        console.log(first_name, last_name)
+        r.pdf.forEach(report=>{
+          if(report.toUpperCase().includes(first_name) && report.toUpperCase().includes(`${last_name},`)){
+            console.log(report)
+          }
+        })
+      }
+    })
+  })
+}
 
-getTexasCandidateNames({year:2020})
+getCandidateMoney();
