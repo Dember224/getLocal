569954a20e97b8f9719ca515449cc3fc6de70af4
Antoine <Dember224@gmail.com>
2021-03-15 00:58:57 -0400

    Creating alabama and adding stateName to Mississippi

---
+++ Alabama/alabamaSearch.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+// 'https://fcpa.alabamavotes.gov/PublicSite/SearchPages/PoliticalRaceSearch.aspx?tb=politicalracesearch'
+
+// _ctl0:Content:ddlElection: 169
+// _ctl0:Content:ddlOffice: 19
+// _ctl0:Content:ddlDistrict: 46
+// _ctl0:Content:ddlParty:ucddlParty: 2
+// _ctl0:Content:ddlYearToShow: 0
+// _ctl0:Content:btnSearch: Search
