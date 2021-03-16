f08d6c861d05846691c5b812903644f01b7da1c5
Antoine <Dember224@gmail.com>
2021-03-16 01:01:31 -0400

    Searching for the elements that will have the ids needed to pickup candidates.

--- Alabama/alabamaSearch.js
+++ Alabama/alabamaSearch.js
@@ -1,6 +1,29 @@
+const request = require('request');
+const cheerio = require('cheerio')
+
+const getSearchSettings = function(){
+  request({
+    uri:'https://fcpa.alabamavotes.gov/PublicSite/SearchPages/PoliticalRaceSearch.aspx?tb=politicalracesearch',
+    qs:{
+      tb: 'politicalracesearch'
+    },
+    json:true,
+    headers: {
+      'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36",
+      'content-type': 'application/x-www-form-urlencoded',
+
+    }
+  },(e,r,b)=>{
+    if(e) return e;
+    const $ = cheerio.load(b);
+    const elements = $('.md-form').text()
+    console.log(elements);
+  })
+}
 
 
 
+getSearchSettings()
 
 
 // 'https://fcpa.alabamavotes.gov/PublicSite/SearchPages/PoliticalRaceSearch.aspx?tb=politicalracesearch'
