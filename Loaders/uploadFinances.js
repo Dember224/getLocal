b7542aa86e44bb0fc39fb6a77c05f0c3dc99d05e
Antoine <Dember224@gmail.com>
2021-04-18 20:15:29 -0400

    Adding deduping and large records mechanisms to the db loader.

--- Loaders/uploadFinances.js
+++ Loaders/uploadFinances.js
@@ -1,50 +1,59 @@
 const dbConfig = require('./dbConfig.js');
 const { Pool, Client } = require('pg');
 const async = require('async');
+const pgp = require('pg-promise');
+const format = require('pg-format');
 
 const pool = new Pool(dbConfig.config)
 
-const loadFinanceArray = async function(finance_array){
-  await pool.connect((e,client,release)=>{
-    async.mapSeries(finance_array, (money_object,cb)=>{
-      if(e) return e;
-      console.log('Money object logged', money_object)
-      const name = money_object.name;
-      const office = money_object.office;
-      const state = money_object.state;
-      const district = money_object.district;
-      const contributions = money_object.contributions;
-      const expenditures = money_object.expenditures;
-      const asOf = money_object.asOf;
-      const election_year = money_object.election_year;
-      const election_type = money_object.election_type;
-      const text = 'INSERT INTO campaign_finance(name,office, state, district, contributions, expenditures, asOf, election_year, election_type) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *';
-      const values = [
-        name,
-        office,
-        state,
-        district,
-        contributions,
-        expenditures,
-        asOf,
-        election_year,
-        election_type
-      ];
-      client.query(text,values,(e,r)=>{
-        if(e) return e;
-        console.log("These are the values", values)
-        if(r.length === 0) {
-          console.log("The method didn't load anything")
-          return cb(null,r);
-        };
-        return cb(null, r);
-      })
-
-  }, (e,r)=>{
-    if(e) return e;
-    release()
-    return r;
+const loadFinanceArray = function(finance_array){
+  const length = finance_array.length
+  let place = 0;
+  async.map(finance_array, (money_object,cb)=>{
+    const name = money_object.name;
+    const office = money_object.office;
+    const state = money_object.state;
+    const district = money_object.district;
+    const contributions = parseFloat(money_object.contributions);
+    const expenditures = parseFloat(money_object.expenditures);
+    const asOf = money_object.asOf;
+    const election_year = money_object.election_year;
+    const election_type = money_object.election_type;
+    const name_year = money_object.name_year
+    const values = [
+      name,
+      office,
+      state,
+      district,
+      contributions,
+      expenditures,
+      asOf,
+      election_year,
+      election_type,
+      name_year
+    ];
+    return cb(null, values)
+    console.log(place, "records loaded")
+    place += 1;
+}, (e,r)=>{
+  if(e) return e;
+  console.log("records loaded:", r);
+  const text = 'INSERT INTO campaign_finance(name,office, state, district, contributions, expenditures, asOf, election_year, election_type,name_year) VALUES %L ON CONFLICT (name_year) DO NOTHING RETURNING *';
+  const values = r;
+  pool.connect()
+  .then(client =>{
+    return client
+    .query(format(text, values))
+    .then(res=>{
+      console.log(res.rows);
+      client.release()
     })
+    .catch(e=>{
+      console.log(e)
+      client.release()
+    })
+  })
+  return r;
   })
 }
 const loadFinanceData = function(callData){
