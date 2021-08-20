cfa1a22976bb18a68ed84a8abfaeeb984688fc02
Antoine <Dember224@gmail.com>
2021-08-20 17:59:06 -0400

    Creating sample queries for the front end to display when ready.

---
+++ Loaders/showData.js
@@ -0,0 +1,23 @@
+const dbConfig = require('./dbConfig.js');
+const { Pool, Client } = require('pg');
+
+const pool = new Pool(dbConfig.config);
+
+const queryOverDrawn = function(){
+  const query = 'Select name, state, office, contributions, expenditures, expenditures - contributions as shortfall from campaign_finance where expenditures > contributions and expenditures is not null and contributions is not null';
+  pool.connect()
+  .then(client =>{
+    return client
+    .query(query)
+    .then(results =>{
+      client.release()
+      console.log(results.rows)
+    })
+  })
+  .catch(e=>{
+    console.log(e);
+    client.release();
+  })
+}
+
+queryOverDrawn()
