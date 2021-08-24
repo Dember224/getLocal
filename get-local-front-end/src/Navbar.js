92624a42beff79b3fff3dc4a7ccf71ae99c85cb4
Antoine <Dember224@gmail.com>
2021-08-24 03:16:37 -0400

    Adding the about us section to Navbar and page

--- get-local-front-end/src/Navbar.js
+++ get-local-front-end/src/Navbar.js
@@ -6,6 +6,7 @@ import { TabContext } from '@material-ui/lab';
 import TabList from '@material-ui/lab/TabList';
 import TabPanel from '@material-ui/lab/TabPanel';
 import {LoadShortfallGrid} from './shortfallgrid';
+import {Explainer} from './explainer'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,12 +29,12 @@ export function Navbar() {
         <AppBar position="static">
           <TabList onChange={handleChange} aria-label="simple tabs example">
             <Tab label="Data" value="1" />
-            <Tab label="Item Two" value="2" />
+            <Tab label="About Us" value="2" />
             <Tab label="Item Three" value="3" />
           </TabList>
         </AppBar>
         <TabPanel value="1"><LoadShortfallGrid/></TabPanel>
-        <TabPanel value="2">Item Two</TabPanel>
+        <TabPanel value="2"><Explainer/></TabPanel>
         <TabPanel value="3">Item Three</TabPanel>
       </TabContext>
     </div>
