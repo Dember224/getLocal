48d3b14424310ffc66aa1ddef0dcfa03cfe245ab
Antoine <Dember224@gmail.com>
2021-08-24 17:44:02 -0400

    updating my explanations of our processes.

---
+++ get-local-front-end/src/shortfallexplainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export function ShortfallExplained(){
+
+  return (
+    <div>
+      <h1>Shortfall</h1>
+      <p style={{width:"60%", "text-align":"center", margin:"auto"}}>
+        Shortfall is our way of saying that a candidate out-spent their contributions.
+        This isn't necessarily the same thing as campaign debt as there are several ways to generate a shortfall. A person could, for example be independenly wealthy and financing their campaings out of pocket rather than through donors.
+        Taken with other information shortfall can be helpful in determining who is struggling to cover their expenses.
+      </p>
+    </div>
+  )
+}
