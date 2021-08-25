bb2df706ed36df4882c69cb7013c1e8319991430
Antoine <Dember224@gmail.com>
2021-08-24 22:23:51 -0400

    Adding contact information and some formatting

---
+++ get-local-front-end/src/contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function ContactInfo(){
+
+  return(
+    <div className="Contacts">
+      Email us:<a href = "mailto: campaignbucs@gmail.com">campaginbucs@gmail.com</a>
+    </div>
+  )
+}
