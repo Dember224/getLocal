4b6dde3ed9ed6085f244155e61b6f5874ea42bcc
Antoine <Dember224@gmail.com>
2021-08-26 00:02:31 -0400

    updating the API to render queries for state total spending and average spending.

--- getLocalApi/app.js
+++ getLocalApi/app.js
@@ -3,9 +3,17 @@ const app = express()
 const port = process.env.PORT || 4000;
 const dataRouter = require('./router.js');
 
-app.use('/shortFall', dataRouter);
+app.use('/', dataRouter.dataRouter);
 
 app.get('/shortfall', (req, res, next)=>{
+  res.send()
+})
+
+app.get('/totals', (req, res,next)=>{
+  res.send()
+})
+
+app.get('/averages', (req, res, next)=>{
   res.send()
 })
 
--- getLocalApi/router.js
+++ getLocalApi/router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dataRouter = express.Router();
 const  queries = require('../Loaders/showData.js');
 
-dataRouter.get('/', (req, res, next)=>{
+dataRouter.get('/shortfall', (req, res, next)=>{
   queries.standardQuery(queries.sql.shortFall,(e, over_drawn_candidates)=>{
     if(e) res.status(404).send({name:'this didnt work'});
     if(over_drawn_candidates.length){
@@ -13,4 +13,28 @@ dataRouter.get('/', (req, res, next)=>{
   })
 })
 
-module.exports = dataRouter;
+dataRouter.get('/totals', (req, res, next)=>{
+  queries.standardQuery(queries.sql.stateTotals, (e, state_finance_totals)=>{
+    if(e) res.status(404).send({state:'this didnt work'});
+    if(state_finance_totals.length){
+      res.send(state_finance_totals)
+    } else {
+      res.status(404).send({state:'State data isnt returning. Check showData, or the pg database.'})
+    }
+  })
+})
+
+dataRouter.get('/averages', (req, res, next)=>{
+  queries.standardQuery(queries.sql.averages, (e, state_finance_averages)=>{
+    if(e) res.status(404).send({state:'this didnt work'});
+    if(state_finance_averages.length){
+      res.send(state_finance_averages)
+    } else {
+      res.status(404).send({state: 'State data isnt returning. Check showData, or the pg database.'})
+    }
+  })
+})
+
+module.exports = {
+  dataRouter
+};
