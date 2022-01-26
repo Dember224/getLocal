7f1ec0a1284e5967e74eb1ed63eed3c6c8f967ac
Antoine <Dember224@gmail.com>
2022-01-25 19:04:50 -0500

    adding Population cards

---
+++ get-local-front-end/src/charts/mascCard.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    width: 230,
+    height:200,
+    margin:"10px",
+    background:'linear-gradient(45deg, #3F51B5 30%, #1E88E5 90%)',
+    color:'white',
+    display:'inline-block'
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+export function RenderMascCard(props){
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography variant="h5" component="h2">
+          Male<br /> Population
+        </Typography>
+        <Typography variant="h2" component="p">
+          {props.male_population}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
--- get-local-front-end/src/charts/profileData.js
+++ get-local-front-end/src/charts/profileData.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {RenderFemCard} from './femCard';
+import {RenderMascCard} from './mascCard'
 
 
 //work around this by using the census methods in the API. I should have never tried it this way in the first place.
 //Put it in the API and pass back the data in the response. React is going to complain about improting methods from outside src.
 export function RenderProfile(props){
   const [censusData, setCensusData] = useState({});
-  const [lookP, setLookP] = useState(props)
+  const [lookP, setLookP] = useState(props);
+  const [femalePopulation, setFemalePopulation] = useState(0);
+  const [malePopulation, setMalePopulation] = useState(0);
 
   useEffect(()=>{
     setLookP(props)
@@ -15,14 +19,19 @@ export function RenderProfile(props){
     axios.get(`http://localhost:4000/profile/${lookP.choosenState}/${lookP.district}/${lookP.office}`)
       .then((response)=>{
         setCensusData(response)
-
+        setFemalePopulation(response.data.female_population)
+        setMalePopulation(response.data.male_population)
       })
   },[lookP.district])
 
 return(
   <div>
+  <div>
+    <RenderFemCard female_population={femalePopulation} />
+    <RenderMascCard male_population={malePopulation} />
+  </div>
     {JSON.stringify(lookP)}
-    {JSON.stringify(censusData)}
+    {JSON.stringify(censusData.data)}
   </div>
 )
 
