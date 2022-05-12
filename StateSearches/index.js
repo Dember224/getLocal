99677a6de11ec878a2fcdbf341e9807521b90ad8
Antoine <Dember224@gmail.com>
2022-05-12 19:56:05 -0400

    Adding party to NC, and making NC function calls accessible to the root index file.

--- StateSearches/index.js
+++ StateSearches/index.js
@@ -1,9 +1,11 @@
 const virginia = require('./Virginia');
 const texas = require('./Texas');
 const ohio = require('./Ohio');
+const north_carolina = require('./NorthCarolina');
 
 module.exports ={
   virginia,
   texas,
-  ohio
+  ohio,
+  north_carolina
 }
