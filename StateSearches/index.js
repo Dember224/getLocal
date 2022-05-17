9e07232c88685e274eed9e6d20c8dbc0bde4b151
Antoine <Dember224@gmail.com>
2022-05-16 22:23:57 -0400

    Adding the new election results methods to the cli handler.

--- StateSearches/index.js
+++ StateSearches/index.js
@@ -2,10 +2,12 @@ const virginia = require('./Virginia');
 const texas = require('./Texas');
 const ohio = require('./Ohio');
 const north_carolina = require('./NorthCarolina');
+const alabama = require('./Alabama');
 
 module.exports ={
   virginia,
   texas,
   ohio,
-  north_carolina
+  north_carolina,
+  alabama
 }
