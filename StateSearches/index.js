f787e76eb3764ee128931bf2409967619f416bcd
Antoine <Dember224@gmail.com>
2022-05-17 20:54:58 -0400

    Adding party getters into the California logic. Making CA callable from the root index file.

--- StateSearches/index.js
+++ StateSearches/index.js
@@ -3,11 +3,13 @@ const texas = require('./Texas');
 const ohio = require('./Ohio');
 const north_carolina = require('./NorthCarolina');
 const alabama = require('./Alabama');
+const california = require('./California');
 
 module.exports ={
   virginia,
   texas,
   ohio,
   north_carolina,
-  alabama
+  alabama,
+  california
 }
