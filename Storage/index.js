7b179298750c60540916d7065f853d69c76e2446
Antoine <Dember224@gmail.com>
2022-07-11 21:19:42 -0400

    Adding candidate search view creation before the candidate search model.

--- Storage/index.js
+++ Storage/index.js
@@ -12,11 +12,11 @@ async function getModels() {
   const db_url = process.env.DB_URL;
     const sequelize = new Sequelize(db_uri, {
       dialect:'postgres',
-    //   dialectOptions: {
-    //     ssl: {
-    //       rejectUnauthorized: false
-    //     }
-    //   }
+      dialectOptions: {
+        ssl: {
+          rejectUnauthorized: false
+        }
+      }
     });
 
     const State = sequelize.define("State", {
@@ -306,31 +306,61 @@ async function getModels() {
         foreignKey: 'candidacy_id'
     });
 
-    // const CandidateSearch = sequelize.define('CandidateSearch', {
-    //   candidacy_id: {
-    //       type: DataTypes.INTEGER,
-    //       primaryKey: true
-    //     },
-    //     candidate_id: {
-    //         type: DataTypes.INTEGER,
-    //         references: {
-    //             model: Candidate,
-    //             key: 'candidate_id'
-    //         }
-    //     },
-    //     first_name: DataTypes.STRING,
-    //     middle_name: DataTypes.STRING,
-    //     last_name: DataTypes.STRING,
-    //     party: DataTypes.STRING,
-    //     state_name: DataTypes.STRING,
-    //     district: DataTypes.INTEGER,
-    //     chamber_level:DataTypes.INTEGER,
-    //     year:DataTypes.INTEGER,
-    //     election_type:DataTypes.STRING
-    //     }, {
-    //         tableName: 'CandidateSearch',
-    //         timestamps:false
-    // })
+    const candidate_search_query = `
+      Create or replace View "CandidateSearch" as
+      Select
+      c.candidacy_id,
+      c.candidate_id,
+      Lower(cd.first_name) as first_name,
+      Lower(cd.middle_name) as middle_name,
+      Lower(cd.last_name) as last_name,
+      cd.party,
+      s.name as state_name,
+      d.number as district,
+      ch.level as chamber_level,
+      e.year,
+      e.type as election_type
+      from "Candidacies" c
+      left join "Candidates" cd
+      on cd.candidate_id = c.candidate_id
+      left join "Elections" e
+      on e.election_id = c.election_id
+      left join "Offices" o
+      on e.office_id = o.office_id
+      left join "Districts" d
+      on d.district_id = o.district_id
+      left Join "Chambers" ch
+      on ch.chamber_id = d.chamber_id
+      left Join "States" s
+      on s.state_id = ch.state_id
+    `;
+    const cs = await sequelize.query(candidate_search_query);
+
+    const CandidateSearch = sequelize.define('CandidateSearch', {
+      candidacy_id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true
+        },
+        candidate_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: Candidate,
+                key: 'candidate_id'
+            }
+        },
+        first_name: DataTypes.STRING,
+        middle_name: DataTypes.STRING,
+        last_name: DataTypes.STRING,
+        party: DataTypes.STRING,
+        state_name: DataTypes.STRING,
+        district: DataTypes.INTEGER,
+        chamber_level:DataTypes.INTEGER,
+        year:DataTypes.INTEGER,
+        election_type:DataTypes.STRING
+        }, {
+            tableName: 'CandidateSearch',
+            timestamps:false
+    })
 
     // for now, just build it
     for(let name in sequelize.models) {
