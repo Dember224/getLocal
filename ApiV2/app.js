b7c963f3c540d52316d77001055ccf311bd0cb09
Antoine <Dember224@gmail.com>
2022-07-18 22:24:06 -0400

    Making cool new stuff accessible through the API. Mainly the sorting races by closeness of votes.

--- ApiV2/app.js
+++ ApiV2/app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = process.env.PORT || 4000;
 // const dataRouter = require('./router.js');
 const cors = require('cors')
+const analytics = require('../Analysis/retrieveData.js');
 
 app.use(cors());
 // app.use('/', dataRouter.dataRouter);
@@ -12,6 +13,11 @@ app.get('/', (req,res,next)=>{
   res.send()
 })
 
+app.get('/closestStateRaces/:state/:year', (req,res)=>{
+  analytics.closest_races_by_state(req.params.state, req.params.year, 10).then((r)=>{
+    res.send(r)
+  })
+})
 // app.get('/profile', (req, res,next)=>{
 //   res.send()
 // })
