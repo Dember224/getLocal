7eb0c3c0d2b31bd066d2acc4832dcf971f54858e
Antoine <Dember224@gmail.com>
2022-11-21 20:50:25 -0500

    preparing to push the update to have gitLocal suggest from a state at random.

--- getLocalHubs/src/controllers/analyticsHandlers.js
+++ getLocalHubs/src/controllers/analyticsHandlers.js
@@ -6,7 +6,7 @@ dotenv.config()
 export default async function getSuggestions(){
   try{
     const base_url = 'https://hubs-api.herokuapp.com/'
-    const res = await axios.get(`${base_url}suggestion/pennsylvania`)
+    const res = await axios.get(`${base_url}random_state_suggestion`)
     return res['data']
   } catch(e) {
     console.log(e);
