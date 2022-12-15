6244e79f128aa8ecb699f4d081a22e8d024f39fe
Antoine <Dember224@gmail.com>
2022-12-14 20:20:26 -0500

    Changing the load files to loop through an array of live states rather than running individual commands in github actions.

--- loader.js
+++ loader.js
@@ -9,6 +9,7 @@ const office = argv.office;
 const report = argv.report;
 const election_type = argv.election_type;
 const district = argv.district;
+const loadGoLiveStates = argv.loadGoLiveStates;
 
 const ElectionResultsLoader = require('./ElectionResults/load');
 const elections = require('./ElectionResults/pull');
@@ -16,11 +17,14 @@ const getStorage = require('./Storage');
 const stateSearches = require('./StateSearches');
 const CampaignFinanceLoader = require('./Finances/load');
 const CensusDataLoader = require('./censusTools/load');
-const census_tools = require('./censusTools/index')
+const census_tools = require('./censusTools/index');
+const go_live_states = require('./miscellaneousVariables');
 
 
-async function loadElectionResults(storage) {
 
+
+async function loadElectionResults(storage, final) {
+  try{
     const upper = await elections.getStateElectionHistoryForLevel({state,level:'upper'});
     const lower = await elections.getStateElectionHistoryForLevel({state,level:'lower'});
 
@@ -30,7 +34,14 @@ async function loadElectionResults(storage) {
 
 
     const loader = new ElectionResultsLoader(storage.models);
-    await loader.loadElectionResults(all);
+
+    await loader.loadElectionResults(all, final);
+    return;
+  } catch(e){
+    console.log('load Election Results Error starting in the main load file:', e)
+  }
+
+
 }
 
 const callData = {
@@ -41,34 +52,39 @@ const callData = {
   report
 }
 
-async function loadFinanceResults(storage){
-  console.log('Finance results called')
-  result = stateSearches[state]['getFinanceData'](callData, async (e,r)=>{
-    if(e){
-      throw new Error("Error retrieving finance results message: "+ e)
-    } else {
-      await loader.loadCampaignFinances(r)
-    }
-  })
-
-  isPromise = result?.then;
-  const loader = new CampaignFinanceLoader(storage);
-
-  if(isPromise){
-    result.then(async (r)=>{
-      await loader.loadCampaignFinances(r);
+async function loadFinanceResults(storage, final){
+  try{
+    console.log('Finance results called')
+    result = stateSearches[state]['getFinanceData'](callData, async (e,r)=>{
+      if(e){
+        throw new Error("Error retrieving finance results message: "+ e)
+      } else {
+        await loader.loadCampaignFinances(r)
+      }
     })
+  
+    isPromise = result?.then;
+    const loader = new CampaignFinanceLoader(storage);
+  
+    if(isPromise){
+      result.then(async (r)=>{
+        await loader.loadCampaignFinances(r, final);
+      })
+    }
+  } catch(e){
+    console.log('Error beginning in load Finance Results in the main loader:', e)
   }
+
 }
 
 async function loadCensusData(storage){
   try{
     console.log('census loader called');
-    const loader = new CensusDataLoader(storage.models);
+    const loadup = new CensusDataLoader(storage.models);
   
     const census_data = await census_tools(year);
   
-    await loader.loadCensusData(census_data);
+    await loadup.loadCensusData(census_data);
   }catch(e){
     console.log(e)
   }
@@ -76,8 +92,8 @@ async function loadCensusData(storage){
 }
 
 
-async function loader() {
-
+async function loader(final) {
+  try {
     const isLoadTypeElections = await loadType == 'elections';
     const isLoadTypeFinance = await loadType == 'finance';
     const isLoadTypeCensus = await loadType == 'census';
@@ -88,6 +104,7 @@ async function loader() {
     if(isLoadTypeElections) {
 
         await loadElectionResults(storage);
+        return;
 
     } else if(isLoadTypeFinance){
 
@@ -97,14 +114,71 @@ async function loader() {
       if (state_split.length == 2){
         state = `${state_split[0]}_${state_split[1]}`
       }
-      await loadFinanceResults(storage.models)
+      await loadFinanceResults(storage.models, final);
+      
       return;
       } else if (isLoadTypeCensus){
 
         await loadCensusData(storage);
+        return;
 
       } else {
           throw new Error("invalid loadType: "+loadType);
       }
-} //honestly this entire thing should be in a try catch block. The error handling in this file is kinda bad. 
-loader();
+  } catch(e){
+    throw new Error(e)
+  }
+
+
+
+
+
+} 
+
+
+async function loadData(){
+  const storage = await getStorage();
+  
+  if(!loadGoLiveStates){
+    try{
+      await loader();
+      return;
+    }catch(e){
+      console.log('Single load fail in the main loader file', e)
+    }
+    
+  } else {
+
+    try{
+      for await (live_state of go_live_states){
+        state = live_state.state;
+        callData.state = state;
+        callData.election_type =  live_state.election_type;
+        const years =  live_state.years;
+        const index = go_live_states.findIndex(object =>{
+          return object.state === live_state;
+        });
+        const go_live_length = go_live_states.length;
+
+        const final_state = index === go_live_length;
+        setTimeout(async ()=>{
+          for (live_year of years){
+            callData.year = live_year;
+              await loader();
+    
+              continue;
+    
+            
+          }
+        }, 30000)
+        continue;
+    }
+  } catch(e){
+      console.log(`failed to load data for ${callData.state}. Failled with response: ${e}`);
+    }
+
+  }
+  return;
+}
+
+loadData()
---
+++ miscellaneousVariables.js
@@ -0,0 +1,19 @@
+const go_live_states = [
+    {
+        state:'pennsylvania',
+        years:[2022],
+
+    },
+    {
+        state:'arizona',
+        years:[2020]
+    },
+    {
+        state: 'north carolina',
+        years:[2022],
+        election_type:'general',
+        final:true
+    }
+]
+
+module.exports = go_live_states;
