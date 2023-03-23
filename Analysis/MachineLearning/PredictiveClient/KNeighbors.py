3dae7b69bba7b287e37dca30f1b89ae088291784
Antoine <Dember224@gmail.com>
2023-03-23 02:24:35 -0400

    Finishing up the predictor. For now prints results on the test set. Will build logic for checking accuracy next.

--- Analysis/MachineLearning/PredictiveClient/KNeighbors.py
+++ Analysis/MachineLearning/PredictiveClient/KNeighbors.py
@@ -1,5 +1,6 @@
 from masterConnection import DataAccessClient
 from sklearn.neighbors import KNeighborsClassifier
+import math
 
 
 class KNeighborsClient(object):
@@ -9,3 +10,26 @@ class KNeighborsClient(object):
 
     def get_zscore_data(self):
         zscore_data = self.data.execute_query(self.zscore_path)
+        return zscore_data
+    
+    def get_predictions(self):
+        data = self.get_zscore_data()
+        n = data["N"]
+        training_set_size = math.trunc(n * .8)
+        k = math.trunc(data["K"])
+        sample = data["SampleList"]
+        labels = data["LabelList"]
+
+        training_set = sample[:training_set_size]
+        training_labels = labels[:training_set_size]
+
+        test_set = sample[training_set_size:]
+        classifier = KNeighborsClassifier(n_neighbors = k)
+        classifier.fit(training_set, training_labels)
+
+        predictions = classifier.predict(test_set)
+        print(predictions)
+
+machine = KNeighborsClient()
+machine.get_predictions()
+
