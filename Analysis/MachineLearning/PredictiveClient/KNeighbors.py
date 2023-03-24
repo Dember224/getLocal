b155384ef6be678fefc2e6c7d81a4044f78dc33c
Antoine <Dember224@gmail.com>
2023-03-23 20:23:56 -0400

    Checking the accuracy of my KNeighbors model. It's not very good. =(

--- Analysis/MachineLearning/PredictiveClient/KNeighbors.py
+++ Analysis/MachineLearning/PredictiveClient/KNeighbors.py
@@ -1,5 +1,6 @@
 from masterConnection import DataAccessClient
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
 import math
 
 
@@ -11,8 +12,17 @@ class KNeighborsClient(object):
     def get_zscore_data(self):
         zscore_data = self.data.execute_query(self.zscore_path)
         return zscore_data
+
+    def check_correctness(self,labels, guesses):
+        return_dict =  {
+            "Accuracy":accuracy_score(labels, guesses),
+            "Recall": recall_score(labels, guesses),
+            "Precision": precision_score(labels, guesses),
+            "F1":f1_score(labels, guesses)
+        }
+        return return_dict
     
-    def get_predictions(self):
+    def get_general_sample_predictions(self):
         data = self.get_zscore_data()
         n = data["N"]
         training_set_size = math.trunc(n * .8)
@@ -23,13 +33,16 @@ class KNeighborsClient(object):
         training_set = sample[:training_set_size]
         training_labels = labels[:training_set_size]
 
+        test_labels= labels[training_set_size:]
+
         test_set = sample[training_set_size:]
         classifier = KNeighborsClassifier(n_neighbors = k)
         classifier.fit(training_set, training_labels)
 
         predictions = classifier.predict(test_set)
         print(predictions)
+        print(self.check_correctness(test_labels,predictions))
 
 machine = KNeighborsClient()
-machine.get_predictions()
+machine.get_general_sample_predictions()
 
