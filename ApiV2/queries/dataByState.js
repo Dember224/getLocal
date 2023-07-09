a22aeab9bd59fa5651367170f2adad7fbed0930f
Antoine <Dember224@gmail.com>
2023-07-09 17:50:47 -0400

    Updating query to return fewer duplicate columns. There should only be duplicates due to ACS vintages returning different demographic data over the years now.

--- ApiV2/queries/dataByState.js
+++ ApiV2/queries/dataByState.js
@@ -1,9 +1,41 @@
 const data = `Select 
-cs.*, 
+distinct
+cs.candidacy_id,
+cs.candidate_id,
+cs.first_name,
+cs.middle_name,
+cs.last_name,
+cs.party,
+cs.state_name,
+cs.district,
+cs.year, 
+cs.election_type,
 cf.expenditures, 
 cf.contributions,
 ch.name as "chamber",
-cd.*
+cd.acs_vintage,
+cd.acs_source_path,
+cd.total_population,
+cd.male_population,
+cd.female_population,
+cd.median_age,
+cd.median_male_age,
+cd.median_female_age,
+cd.under_18_population,
+cd.white_population,
+cd.black_population, 
+cd.native_american_population, 
+cd.asian_population, 
+cd.latino_population, 
+cd.pacific_islander_population, 
+cd.citizen_population,
+cd.non_citizen_population,
+cd.married_population, 
+cd.never_married_population, 
+cd.separated_marriage_population, 
+cd.divorced_population,
+cd.median_income,
+cd.other_race_population
 from "States" s
 
 inner join "CandidateSearch" cs
@@ -27,6 +59,6 @@ inner join "CensusData" cd
 on cd.district_id = d.district_id
 
 
-order by cs.last_name `;
+order by cs.last_name ;`;
 
 module.exports = data;
