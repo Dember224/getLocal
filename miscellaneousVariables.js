ad07d2e4e7066685ae650e11f82c8bd35d27db00
Antoine <Dember224@gmail.com>
2023-07-13 18:18:25 -0400

    Adding several states to go live states for auto loads.

--- miscellaneousVariables.js
+++ miscellaneousVariables.js
@@ -2,17 +2,39 @@ const go_live_states = [
     {
         state:'pennsylvania',
         years:[2022],
+        election_type:'general'
 
     },
     {
         state:'arizona',
-        years:[2020]
+        years:[2020],
+        election_type:'general'
+    },
+    {
+        state: 'georgia',
+        years:[2023],
+        election_type:'general'
+    },
+    {
+        state:'kentucky',
+        years:[2023],
+        election_type:'general'
+    },
+    {
+        state: 'new jersey',
+        years: [2023],
+        election_type:'general'
     },
     {
         state: 'north carolina',
         years:[2022],
-        election_type:'general',
-        final:true
+        election_type:'general'
+    },
+    {
+        state: 'virginia',
+        years: [2022,2023],
+        election_type: 'general',
+        final: true
     }
 ]
 
